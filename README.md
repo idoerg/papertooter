5050655ec6f553fa9d94e010598227251c10ad61
idoerg <idoerg@gmail.com>
2022-12-14 15:19:27 -0600

    new file:   .gitignore 	modified:   README.md 	modified:   papertoot.py

--- README.md
+++ README.md
@@ -2,14 +2,24 @@
 A script to post manuscripts from BioRxiv to Mastodon using a unique hashtag based on the manuscript DOI.
 
 ## Purpose
-The purpose of this program is to post ("toot") a [BioRxiv](https://www.biorxiv.org/) preprint on the Mastodon social network. You would need a Mastodon account, and you would need to connect it to Mastodon using Mastodon.py. To post the bioarxiv preprint you would run the following from the command line:
 
-`$ python papertoot.py http://biorxiv.org/paper_url mastodon_client_credentials_file`
+The purpose of this program is to post ("toot") a [BioRxiv](https://www.biorxiv.org/),
+[medRxiv](https://www.medrxiv.org) or [arXiv](https://arxiv.org) preprint on the Mastodon social network.
+You would need a Mastodon account, and you would need to connect it to Mastodon using Mastodon.py. To post
+the bioarxiv preprint you would run the following from the command line:
 
-This will create a post on Mastodon using your account with the paper title, a hashtag based on the paper DOI, and a link to the paper. 
+`$ python papertoot.py -c mastodon_client_credentials_file http://biorxiv.org/paper_url `
+
+This will create a post on Mastodon using your account with the paper title, a hashtag based on the paper
+DOI, and a link to the paper. 
 
 ### Hashtag
-The hashtag is based on the paper DOI. In Biorxiv the DOI is composed of alphanumberic characters, slashes and periods. Because hashtags do not allow persiods and slashes, a `/` is converted to a `__` (double underscore) and a `.` is converted to a `_` (single underscore). This allows for a unique tagging of the manuscript. So the DOI `DOI:10.1101/2022.11.28.518265` will be converted to `#10_1101__2022_11_28_518265`.
+
+The hashtag is based on the paper DOI. In Biorxiv the DOI is composed of alphanumeric characters, slashes
+and periods. Because hashtags do not allow periods and slashes, a `/` is converted to a `__` (double
+underscore) and any other non-alphanumeric os underscore character is converted to a `_` (single
+underscore). This allows for a (mostly)  tagging of the manuscript. So the DOI `DOI:10.1101/2022.11.28.518265`
+will be converted to `#10_1101__2022_11_28_518265`.
 
 
 ### Dependencies
@@ -20,9 +30,21 @@ The hashtag is based on the paper DOI. In Biorxiv the DOI is composed of alphanu
 ### Prerequsites
 You will need 
  + A Mastodon account
- + Mastodon user for the account wiht credentials so you could toot from the Python script. To learn how to generate the credentials, see the top of the README file in [Mastodon.py](https://github.com/halcy/Mastodon.py)
+ + Mastodon user credentials so you could toot from the Python script. To learn how to generate the
+   credentials, see the top of the README file in [Mastodon.py](https://github.com/halcy/Mastodon.py)
  
  
+### Command line options:
+
+```
+-h, --help               show this help message and exit
+-v, --verbose            debug information mostly
+-s, --silent             silent: do not toot
+-c CREDS, --creds CREDS  Mastodon user credentials file
+
+```
+
+
 ### Example:
 
 ![Toot](mytoot.png)
--- papertoot.py
+++ papertoot.py
@@ -1,39 +1,110 @@
 #!/usr/bin/env python
 
 import sys
-from bs4 import BeautifulSoup as BS
-import requests
+import re 
+import argparse 
+from bs4 import BeautifulSoup as BS 
+import requests 
 from mastodon import Mastodon
+
+##############################################################################
+#
+# Copyright 2022 Iddo Friedberg
+# 
+# This program is free software: you can redistribute it and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with this program. If
+# not, see <https://www.gnu.org/licenses/>. 
+#
+##############################################################################
 
 def doi_to_hashtag(doi):
     '''
     converts a DOI to hashtag
-    _ for a .
+    _ for all non alphanumeric or underscore characters.
     __ for a  /
     '''
-    hashtag = doi.replace('.','_')
-    hashtag = hashtag.replace('/','__')
+    # replace "/" with "__" (two underscores)
+    hashtag = re.sub('\/','__', doi)
+    #replace all other non-alphanumeric an non-underscore characters with "_"
+    hashtag = re.sub('[^0-9a-zA-Z_]+','_', hashtag)
     hashtag = '#' + hashtag
     return hashtag
 
-def parse_info(url):
-    ''' parses the doi, title, url, given a biorxiv url'''
-    req = requests.get(url)
-    html_doc = req.text
-    soup = BS(html_doc, 'html.parser')
+def parse_biorxiv_info(soup):
+    ''' parses the doi, title, url, given a biorxiv or medrxiv url'''
     doi = soup.find("meta", {"name": "citation_doi"})["content"]
-    # Commented out lines below are for arxiv.org Needs work
     title = soup.find("meta", {"name": "DC.Title"})["content"]
-    # title = soup.find("meta", {"name": "citation_title"})["content"]
     public_url = soup.find("meta", {"name": "citation_public_url"})["content"]
-    # public_url = soup.find("meta", {"name": "citation_pdf_url"})["content"]
+    return (doi, title, public_url)
+
+def parse_arxiv_info(soup):
+    ''' parses the doi, title, url, given an arxiv url'''
+    doi = soup.find("meta", {"name": "citation_doi"})["content"]
+    title = soup.find("meta", {"name": "citation_title"})["content"]
+    public_url = soup.find("meta", {"property": "og:url"})["content"]
     return (doi, title, public_url)
 
 def toot_article(hashtag, title, public_url):
     toot = mastodon.toot(f'{title} {hashtag}\n {public_url}')
 
+def archive_id(url):
+    '''
+    Identify the paper archive. 
+    '''
+    req = requests.get(url)
+    html_doc = req.text
+    site = None
+    soup = BS(html_doc, 'html.parser')
+    tag = soup.find("meta", {"property": "og:site_name"})
+    if tag:
+        if tag["content"].lower() == "arxiv.org":
+            site = "arxiv"
+        elif tag["content"].lower() == "biorxiv":
+            site = "biorxiv"
+        elif tag["content"].lower() == "medrxiv":
+            site = "medrxiv"
+    return site, soup
+ 
+def cli_parser():
+    parser = argparse.ArgumentParser(
+                prog = "papertoot",
+                description = "Toots papers given their URLs")
+    parser.add_argument('url', help="url of paper you want to toot")
+    parser.add_argument('-v', '--verbose', action='store_true', help="debug information mostly") 
+    parser.add_argument('-s', '--silent', action='store_true', help="silent: do not toot") 
+    parser.add_argument('-c', '--creds',  help="Mastodon user credentials file")
+    return parser
+         
 if __name__ == '__main__':
-    doi, title, public_url = parse_info(sys.argv[1])    
-    hashtag = doi_to_hashtag(doi)
-    mastodon = Mastodon(access_token = sys.argv[2])
-    toot_article(hashtag, title, public_url)
+    doi = ''
+    title = ''
+    public_url = ''
+    parser = cli_parser()
+    args = parser.parse_args()
+    site, soup = archive_id(args.url)
+    if site == "biorxiv" or site == "medrxiv":
+        doi, title, public_url = parse_biorxiv_info(soup)
+        hashtag = doi_to_hashtag(doi)
+    elif site == "arxiv":
+        doi, title, public_url = parse_arxiv_info(soup)
+        hashtag = doi_to_hashtag(doi)
+    else:
+        raise ValueError(f"url {url} not identified to be in a covered site (biorxiv, medrxiv, arxiv)")
+    if args.verbose:
+        print(f"doi {doi}")
+        print(f"title {title}")
+        print(f"public url {public_url}")
+        print(f"hashtag {hashtag}")
+    if not args.silent:
+        if args.creds:
+            mastodon = Mastodon(access_token = args.creds)
+            toot_article(hashtag, title, public_url)
+        else: 
+            raise ValueError("You need creds to toot!")
